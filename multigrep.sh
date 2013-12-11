780a783df337f5a5cbb64ff16d902a41d94089e5
Gert Hulselmans <hulselmansgert@gmail.com>
2013-12-11 19:44:04 +0100

    Add support for grepping for the pattern only at certain fields instead of the whole line.
    Add support for searching the patterns only at certain fields instead of
    the whole line. To limit the searching of the patterns to certain field
    a comma separated list can be given to the -f option.
    
    The default field separator is set to TAB, but can be changed with the
    -s option.
    
    The error message are now also written to stderr instead of stdout.
    
    Signed-off-by: Gert Hulselmans <hulselmansgert@gmail.com>

--- multigrep.sh
+++ multigrep.sh
@@ -39,6 +39,10 @@ declare -a input_files;
 declare -i i=0;
 
 
+# By default, search the whole line for the pattern.
+field_numbers=0;
+# Set the default field separator to TAB.
+field_separator='\t';
 
 
 
@@ -46,10 +50,18 @@ declare -i i=0;
 usage () {
     add_spaces="           ${0//?/ }";
 
-    printf "\n%s\n\n%s\n%s\n\n%s\n%s\n\n" \
-           "Usage:     ${0} -g grep_patterns_file [file(s)]" \
+    printf "\n%s\n%s\n%s\n%s\n\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n%s\n%s\n\n" \
+           "Usage:     ${0} -g grep_patterns_file" \
+           "${add_spaces} [-f field_numbers]" \
+           "${add_spaces} [-s field_separator]" \
+           "${add_spaces} [file(s)]" \
            "Arguments:" \
-           "           -g grep_patterns_file    File with patterns to grep for." \
+           "           -f field_numbers         Comma separated list of field numbers." \
+           "                                    If specified, the pattern natching will" \
+           "                                    only occur in those fields instead of" \
+           "                                    in the whole line ( = field_number=0)." \
+           "           -g grep_patterns_file    File with patterns to grep for (required)." \
+           "           -s field_separator       Field separator (default: '\t')." \
            "Purpose:" \
            "           Grep for multiple patterns at once in one or more files.";
 }
@@ -58,14 +70,29 @@ usage () {
 
 
 
+
 # Retrieve passed arguments and filenames.
 until ( [ -z "$1" ] ) ; do
     case "${1}" in
+        -f)          if [ -z "${2}" ] ; then
+                         printf "\nERROR: Parameter '-f' requires comma separated list of fields as argument.\n\n" > /dev/stderr;
+                         exit 1;
+                     else
+                         field_numbers="${2}";
+                         shift 2;
+                     fi;;
         -g)          if [ -z "${2}" ] ; then
-                         printf "\nERROR: Parameter '-g' requires a file with grep patterns as argument.\n\n";
+                         printf "\nERROR: Parameter '-g' requires a file with grep patterns as argument.\n\n" > /dev/stderr;
                          exit 1;
                      else
                          grep_patterns_file="${2}";
+                         shift 2;
+                     fi;;
+        -s)          if [ -z "${2}" ] ; then
+                         printf "\nERROR: Parameter '-s' requires a field separator pattern as argument.\n\n" > /dev/stderr;
+                         exit 1;
+                     else
+                         field_separator="${2}";
                          shift 2;
                      fi;;
         -h)          usage;
@@ -79,7 +106,7 @@ until ( [ -z "$1" ] ) ; do
                      i=i+1;
                      shift 1;;
         *)           if [ ! -e "${1}" ] ; then
-                         printf "\nERROR: Unknown parameter '$1'.\n\n";
+                         printf "\nERROR: Unknown parameter '$1'.\n\n" > /dev/stderr;
                          usage;
                          exit 1;
                      fi
@@ -93,10 +120,10 @@ done
 
 
 if [ -z "${grep_patterns_file}" ] ; then
-    printf "\nERROR: Specify a grep patterns file.\n\n";
+    printf "\nERROR: Specify a grep patterns file.\n\n" > /dev/stderr;
     exit 1;
 elif [ ! -f "${grep_patterns_file}" ] ; then
-    printf "\nERROR: The grep patterns file '%s' could not be found.\n\n" "${grep_patterns_file}";
+    printf "\nERROR: The grep patterns file '%s' could not be found.\n\n" "${grep_patterns_file}" > /dev/stderr;
     exit 1;
 fi
 
@@ -104,6 +131,8 @@ fi
 
 "${AWK}" \
     -v grep_patterns_file="${grep_patterns_file}" \
+    -v field_numbers="${field_numbers}" \
+    -F "${field_separator}" \
     '
     BEGIN {
             # Index for the grep_pattern_array.
@@ -117,16 +146,59 @@ fi
                 # Save pattern from grep pattern file to array.
                 grep_pattern_array[i] = $0;
             }
+
+            # Split field_numbers on comma.
+            nbr_of_fields = split(field_numbers, field_numbers_array, ",");
+
+            if ( nbr_of_fields == 0 ) {
+                # If no field_number is passed, set it to 0 (= whole line).
+                field_numbers_array[1] = 0;
+            } else {
+                for ( field_number_idx in field_numbers_array ) {
+                    # Make an integer of each field number.
+                    field_numbers_array[field_number_idx] = int(field_numbers_array[field_number_idx]);
+
+                    # If it is not an number it will be converted to 0 by awk.
+                    if ( field_numbers_array[field_number_idx] == 0 ) {
+                        # If we need to match the whole line it does not make sense to check individual fields,
+                        # so delete the array and recreate it with only one element set to 0 (= whole line).
+                        delete field_numbers_array;
+                        field_numbers_array[1] = 0;
+                        break;
+                    }
+                }
+            }
     }
     {
-            for (grep_pattern_index in grep_pattern_array) {
-                if ( $0 ~ grep_pattern_array[grep_pattern_index] ) {
-                    # Print the current input line if the pattern matches.
-                    print $0;
-                    
-                    # Go out of the for loop and read next line, when a match
-                    # was found. This prevents reporting the same line multiple
-                    # times when multiple patterns match the current line.
+            # Loop over all selected fields to look for the patterns.
+            for ( field_number_idx in field_numbers_array ) {
+                # Go to the next field number if the current selected field number
+                # is higher than the number of fields of the current line.
+                if ( field_numbers_array[field_number_idx] > NF ) {
+                    continue;
+                }
+
+                # Set content variable to the right field number (= 1 or higher) or the whole line (= 0).
+                content = $field_numbers_array[field_number_idx];
+
+                # Check for each pattern if it matches the selected field.
+                for ( grep_pattern_idx in grep_pattern_array ) {
+                    # Check for the current pattern if it matches the selected field.
+                    if ( content ~ grep_pattern_array[grep_pattern_idx] ) {
+                        # Print the current input line if the pattern matches.
+                        print $0;
+
+                        # Save the last printed line number, to prevent printing the same line more than once.
+                        last_printed_linenumber = NR;
+
+                        # Go out of the grep_pattern_array for loop, so no useless iterations are done.
+                        break;
+                    }
+                }
+
+                # Go out of the field_numbers_array for loop and read next line, when we already printed the
+                # current line (no need to check of another match in the current line in another field).
+                if ( last_printed_linenumber == NR ) {
                     break;
                 }
             }
